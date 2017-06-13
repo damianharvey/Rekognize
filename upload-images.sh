770fa2e133edea968d5ad11c2d6cbd31df660503
damianharvey <damianharvey@gmail.com>
2017-06-13 22:39:30 +1200

    Added upload of images

--- upload-images.sh
+++ upload-images.sh
@@ -1,6 +1,6 @@
 for f in *.*
 do
         echo "Uploading File $f"
-        aws s3 mv $f s3://rekognize-faces
+        aws s3 cp $f s3://rekognize-faces
         aws rekognition index-faces --collection-id faces-collection --image '{"S3Object":{"Bucket":"rekognize-faces","Name":"'$f'"}}' --external-image-id $f
 done
